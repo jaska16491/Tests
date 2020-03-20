a87bb03b3bf850de5ad40485e47b137fe2734f60
jaska16491 <s16491@pjwstk.edu.pl>
2020-03-20 11:37:25 +0100

    drugie

---
+++ XUnitTestProject1/Controllers/BookBorrowsInitTests.cs
@@ -0,0 +1,115 @@
+﻿using Library.Entities;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1.Controllers
+{
+    public class BookBorrowsInitTests
+    {
+
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public BookBorrowsInitTests()
+        {
+            _server = ServerFactory.GetServerInstance();
+            _client = _server.CreateClient();
+
+
+            using (var scope = _server.Host.Services.CreateScope())
+            {
+                var _db = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+
+                _db.BookBorrow.Add(new BookBorrow
+                {
+
+
+                    IdBookBorrow = 200,
+                    IdUser = 200,
+                    IdBook = 200,
+                    BorrowDate = new DateTime(2020, 3, 19),
+                    ReturnDate = new DateTime(2020, 4, 3),
+                    Comments = "Borrowed"
+
+
+                });
+
+                _db.SaveChanges();
+
+            }
+        }
+
+
+        [Fact]
+        public async Task PostBookBorrow()
+        {
+
+            var newBookBorrow = new BookBorrow
+            {
+                IdUser = 200,
+                IdBookBorrow = 201,
+                IdBook = 201,
+                BorrowDate = new DateTime(2020, 3, 19),
+                ReturnDate = new DateTime(2020, 4, 3),
+                Comments = "Borrowed"
+            };
+            var httpResponse = await _client.PostAsync($"{_client.BaseAddress.AbsoluteUri}api/book-borrows", new StringContent(
+                    JsonConvert.SerializeObject(newBookBorrow),
+                    Encoding.UTF8,
+                    "application/json"
+                ));
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var content = await httpResponse.Content.ReadAsStringAsync();
+            var bookBorrow = JsonConvert.DeserializeObject<BookBorrow>(content);
+
+            Assert.True(bookBorrow.IdBook == 201);
+        }
+
+
+
+
+
+        [Fact]
+        public async Task PutBookBorrow()
+        {
+            int BookBorrowId = 200;
+            string newComment = "Returned";
+
+            var updatedBookBorrow = new BookBorrow
+            {
+                IdUser = 200,
+                IdBookBorrow = BookBorrowId,
+                IdBook = 201,
+                BorrowDate = new DateTime(2020, 3, 19),
+                ReturnDate = new DateTime(2020, 4, 3),
+                Comments = newComment
+            };
+            var httpResponse = await _client.PutAsync($"{_client.BaseAddress.AbsoluteUri}api/book-borrows/{BookBorrowId}", new StringContent(
+                    JsonConvert.SerializeObject(updatedBookBorrow),
+                    Encoding.UTF8,
+                    "application/json"
+                ));
+
+            httpResponse.EnsureSuccessStatusCode();
+
+
+            using (var scope = _server.Host.Services.CreateScope())
+            {
+                var _db = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+                Assert.True(_db.BookBorrow.Any(e => e.IdBookBorrow == 200 && e.Comments == newComment));
+            }
+
+
+        }
+    }
+}
---
+++ XUnitTestProject1/Controllers/UsersInitTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Entities;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace XUnitTestProject1.Controllers
+{
+    public class UsersInitTests
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+
+        public UsersInitTests()
+        {
+            _server = ServerFactory.GetServerInstance();
+            _client = _server.CreateClient();
+
+
+            using (var scope = _server.Host.Services.CreateScope())
+            {
+                var _db = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+
+                _db.User.Add(new User
+                {
+                    IdUser = 1,
+                    Email = "jd@pja.edu.pl",
+                    Name = "Daniel",
+                    Surname = "Jabłoński",
+                    Login = "jd",
+                    Password = "ASNDKWQOJRJOP!JO@JOP"
+                });
+
+                _db.SaveChanges();
+
+            }
+        }
+
+
+        [Fact]
+        public async Task GetUsers_200Ok()
+        {
+            var httpResponse = await _client.GetAsync($"{_client.BaseAddress.AbsoluteUri}api/users");
+
+            httpResponse.EnsureSuccessStatusCode();
+            var content = await httpResponse.Content.ReadAsStringAsync();
+            var users = JsonConvert.DeserializeObject<IEnumerable<User>>(content);
+            // using (var scope = _server.Host.Services.CreateScope())
+            // {
+            //     var _db = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+            //     Assert.True(_db.User.Any());
+            // }
+
+            Assert.True(users.Count() == 1);
+            Assert.True(users.ElementAt(0).Login == "jd");
+        }
+
+        [Fact]
+        public async Task GetUsersId()
+        {
+            int userId = 1;
+            var httpResponse = await _client.GetAsync($"{_client.BaseAddress.AbsoluteUri}api/users/{userId}");
+
+            httpResponse.EnsureSuccessStatusCode();
+            var content = await httpResponse.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<User>(content);
+            // using (var scope = _server.Host.Services.CreateScope())
+            // {
+            //     var _db = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+            //     Assert.True(_db.User.Any());
+            // }
+
+            Assert.True(user.IdUser == 1);
+
+        }
+
+        [Fact]
+        public async Task PostUser()
+        {
+            var newUser = new User
+            {
+                IdUser = 90,
+                Email = "pp@pja.edu.pl",
+                Name = "Piotr",
+                Surname = "Puszka",
+                Login = "pp",
+                Password = "DHDHDHDHDDHHDHDH!@!"
+            };
+            var httpResponse = await _client.PostAsync($"{_client.BaseAddress.AbsoluteUri}api/users", new StringContent(
+                    JsonConvert.SerializeObject(newUser),
+                    Encoding.UTF8,
+                    "application/json"
+                ));
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var content = await httpResponse.Content.ReadAsStringAsync();
+            var savedUser = JsonConvert.DeserializeObject<User>(content);
+
+
+            Assert.True(savedUser.Login == "pp");
+
+        }
+    }
+}
